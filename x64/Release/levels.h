9df0735e7ded8b58085aff153266f84ff9fc4d40
Pedro Seixas <pedrodfs15@gmail.com>
2023-05-04 15:43:51 -0400

    Changed FIle Names

---
+++ x64/Release/levels.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef LEVELS_H
+#define LEVELS_H
+
+#include "SFML/Graphics.hpp"
+#include "Ball.h"
+#include "Rope.h"
+#include "Goal.h"
+#include "Obstacle.h"
+#include "TextTransition.h"
+
+bool level(sf::RenderWindow& window);
+void loadLevelTwo(sf::RenderWindow& window);
+void loadLevelThree(sf::RenderWindow& window);
+void loadLevelFour(sf::RenderWindow& window);
+bool transition(sf::RenderWindow& window);
+bool getCompleteStatus();
+
+#endif
